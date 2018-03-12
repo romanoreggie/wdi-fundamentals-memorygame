b153f9a1db16c77a6bf5e5b8e0f394ff81fd0168
romanoreggie <37195521+romanoreggie@users.noreply.github.com>
2018-03-11 21:20:46 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 console.log("Up and running!")
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen","queen","king","king"];
 
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardFour);
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2);
+
+if (cardsInPlay[0] === cardsInPlay[1])
+	alert("You found a match!");
+else if (cardsInPlay[0] !== cardsInPlay[1]) 
+	alert("Sorry, try again.");
+
